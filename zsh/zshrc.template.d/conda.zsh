8e4932c298957c80c3df2d89f2b70e56b43e53c9
Nui Narongwet <narongwet.m@gmail.com>
2024-05-11 21:05:58 +0700

    Add zsh template for conda

---
+++ zsh/zshrc.template.d/conda.zsh
@@ -0,0 +1,19 @@
+# >>> conda initialize >>>
+__conda_setup="$('/opt/conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+        . "/opt/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+function conda-deactivate-all() {
+    while [ ${CONDA_SHLVL:-0} -ne 0 ]; do
+        conda deactivate
+    done
+}
