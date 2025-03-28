c882fb6e510e6a1478002f197ed4aa13e6f4f0b2
Nui Narongwet <narongwet.m@gmail.com>
2025-03-28 16:12:25 +0700

    Add more lua template function

--- template/lua/k8s.lua
+++ template/lua/k8s.lua
@@ -1,11 +1,23 @@
-function string:startswith(start)
-    return self:sub(1, #start) == start
-end
+require "lua-string"
 
 local function transform(content)
     local out = ""
     for line in content:gmatch("([^\n]*)\n") do
-        if not utils.is_comment(line) then
+        local line_no_leading_spaces = line:trimstart()
+        local is_comment_line = line_no_leading_spaces:startswith("#")
+        if not is_comment_line then
+            out = out .. line .. "\n"
+        end
+    end
+    return out
+end
+
+local function transform_rust(content)
+    local out = ""
+    for line in content:gmatch("([^\n]*)\n") do
+        local line_no_leading_spaces = rs.str.trim_start(line)
+        local is_comment_line = rs.str.starts_with(line_no_leading_spaces, "#")
+        if not is_comment_line then
             out = out .. line .. "\n"
         end
     end
@@ -16,4 +28,6 @@ local function identity(content)
     return content
 end
 
+-- return transform
+-- return transform_rust
 return identity
