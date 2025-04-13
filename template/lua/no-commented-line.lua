d5784de6a6bdf06d3f22513b2a584b5a69bd8acc
Nui Narongwet <narongwet.m@gmail.com>
2025-04-13 18:00:59 +0700

    rename no-comment.lua -> no-commented-line

--- template/lua/no-commented-line.lua
+++ template/lua/no-commented-line.lua
@@ -1,14 +1,13 @@
 local function transform(content)
     local buf = rs.StringBuf.with_capacity(#content)
-    for line in rs.String.new(content):lines() do
+    for line in rs.String.new(content):lines_with_terminator() do
         local line_no_leading_spaces = line:trim_start()
         local is_comment_line = line_no_leading_spaces:starts_with("#")
         if not is_comment_line then
             buf:push(line)
-            buf:put_newline()
         end
     end
-    return buf:take_string():trim().val
+    return buf:take_string().val
 end
 
 local function identity(content)
