2bd135ea80861c7ce3c9f81801f95912ddb8781e
Nui Narongwet <narongwet.m@gmail.com>
2025-08-01 03:19:44 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,33 +4,16 @@
 #
 # Compatibility: dash, bash, and zsh
 
-# This make bash not put following commands in history when sourcing this file
-unset HISTFILE
-
 if [ -n "$ZSH_VERSION" ]; then
     # make zsh behave like bash on word spliting
     setopt sh_word_split
 fi
 
 try_start() {
+    local exec_override_shell
     local flags
     local launcher
-    local exec_override_shell
     launcher="$1"
-    shift 1
-
-    exec_override_shell=0
-    if [ -n "$SSH_ORIGINAL_COMMAND" -a "$SHLVL" = 0 ]; then
-        case "$SSH_ORIGINAL_COMMAND" in
-            *sh )
-                # change SHELL to our shell rather than using a default login shell
-                exec_override_shell=1
-                ;;
-            * )
-                eval "$SSH_ORIGINAL_COMMAND"
-                exit $?
-        esac
-    fi
 
     # return if file doesn't exist or broken symlink
     if [ ! -e "$launcher" ]; then
@@ -43,16 +26,27 @@ try_start() {
         return
     fi
 
+    exec_override_shell=0
+    if [ -n "$SSH_ORIGINAL_COMMAND" -a "$SHLVL" = 0 ]; then
+        case "$SSH_ORIGINAL_COMMAND" in
+            *sh )
+                # change SHELL to our shell
+                exec_override_shell=1
+                ;;
+            * )
+                eval "$SSH_ORIGINAL_COMMAND"
+                exit $?
+        esac
+    fi
+
     if [ $exec_override_shell -eq 1 ]; then
         exec "$launcher" exec --set-shell "$SSH_ORIGINAL_COMMAND"
+    else
+        if [ -n "$SSH_CONNECTION" ]; then
+            flags="$flags --motd"
+        fi
+        exec "$launcher" $flags exec -l zsh
     fi
-
-    flags=""
-    if [ -n "$SSH_CONNECTION" ]; then
-        flags="$flags --motd"
-    fi
-
-    exec "$launcher" $flags exec -l zsh
 }
 
 try_start "${NMK_HOME:-$HOME/.nmk}/bin/nmk"
@@ -62,7 +56,17 @@ if [ -x "$global_nmk" ]; then
     try_start "$global_nmk"
 fi
 
-# If this line is reached, we didn't find candidate launcher, fallback to re-execute itself.
+
+
+# If this line is reached, we didn't find candidate launcher
+
+# If command is set, execute it
+if [ -n "$SSH_ORIGINAL_COMMAND" -a "$SHLVL" = 0 ]; then
+    eval "$SSH_ORIGINAL_COMMAND"
+    exit $?
+fi
+
+# fallback to re-execute itself.
 # N.B. MacOs doesn't have procfs, we fallback to /bin/sh
 current_exe=$(readlink -f /proc/$$/exe)
 if [ ! -e "$current_exe" ]; then
