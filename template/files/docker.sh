3231f5454f3bb5bc57b05c21f745945ff074311d
Nui Narongwet <narongwet.m@gmail.com>
2025-08-06 09:15:16 +0700

    Add remote buildkit

--- template/files/docker.sh
+++ template/files/docker.sh
@@ -1,6 +1,9 @@
 # Create a new buildx builder
 docker buildx create --name builder --driver docker-container --use
 
+# Create a new remote builder
+docker buildx create --name=remote --driver=remote
+
 # Create docker context for rootless docker on linux
 docker context create rootless --docker host=unix://$XDG_RUNTIME_DIR/docker.sock
 
