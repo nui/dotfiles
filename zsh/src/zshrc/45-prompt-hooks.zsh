f0c14d15fcdba2254663f60c9a462d00ecd4296d
Nui Narongwet <narongwet.m@gmail.com>
2025-08-07 14:03:11 +0700

    update message on SSH_AUTH_SOCK change

--- zsh/src/zshrc/45-prompt-hooks.zsh
+++ zsh/src/zshrc/45-prompt-hooks.zsh
@@ -16,25 +16,28 @@ _nmk-kubectl-preexec() {
 typeset -g _nmk_update_ssh_socket_tmux_last_check=0
 _nmk-update-ssh-socket-tmux() {
     [[ -n $SSH_AUTH_SOCK && ! -S $SSH_AUTH_SOCK ]] && (( $EPOCHSECONDS - $_nmk_update_ssh_socket_tmux_last_check > 60 )) && {
-        echo >&2 "prompt-hook ($0): update environment from tmux session"
         eval "$(tmux show-environment -s)"
+        echo >&2 "prompt-hook ($0): environment updated from tmux session"
         _nmk_update_ssh_socket_tmux_last_check=$EPOCHSECONDS
     }
 }
 
 _nmk-update-ssh-socket-vscode() {
     local -a sockets
-    local new_socket
+    local cur_socket
     [[ -n $SSH_AUTH_SOCK && ! -S $SSH_AUTH_SOCK ]] && {
+        cur_socket=$SSH_AUTH_SOCK
         # see https://zsh.sourceforge.io/Doc/Release/Expansion.html#Glob-Qualifiers
         # O = files owned by the effective user ID
         # om = sort by last modification, youngest first
         sockets=(/tmp/vscode-ssh-auth-*(Uom))
-        [[ ${#sockets} -ge 1 ]] && {
-            new_socket=${sockets[1]}
-            echo >&2 "$0: update SSH_AUTH_SOCK to $new_socket"
-            export SSH_AUTH_SOCK=$new_socket
-        }
+        for socket in ${sockets[@]}; do
+            if [[ -S $socket ]]; then
+                export SSH_AUTH_SOCK=$socket
+                echo >&2 "$0: SSH_AUTH_SOCK changed from $cur_socket to $socket"
+                return 0
+            fi
+        done
     }
 }
 
@@ -44,11 +47,15 @@ _nmk-update-ssh-socket-vscode() {
 }
 
 [[ -n $SSH_AUTH_SOCK ]] && {
+    # tmux session on remote server
     if [[ -n $TMUX && -n $SSH_CONNECTION ]]; then
+        # register a hook running on every prompt line
         _nmk_precmd_functions+=_nmk-update-ssh-socket-tmux
     elif [[ $TERM_PROGRAM == vscode ]]; then
         if [[ $SSH_AUTH_SOCK == /tmp/vscode-ssh-auth-* ]]; then
+            # vscode on remote server or devcontainer
             if [[ $REMOTE_CONTAINERS == true ]] || [[ -n $SSH_CONNECTION ]]; then
+                # runs on shell start (new terminal/revived terminal)
                 _nmk-update-ssh-socket-vscode
             fi
         fi
