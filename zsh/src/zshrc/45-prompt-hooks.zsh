f3e91a29bafad51e8566d566092ed98bfb41aad1
Nui Narongwet <narongwet.m@gmail.com>
2025-08-07 21:18:57 +0700

    vscode ssh socket check running on prompt rendering

--- zsh/src/zshrc/45-prompt-hooks.zsh
+++ zsh/src/zshrc/45-prompt-hooks.zsh
@@ -15,29 +15,58 @@ _nmk-kubectl-preexec() {
 
 typeset -g _nmk_update_ssh_socket_tmux_last_check=0
 _nmk-update-ssh-socket-tmux() {
-    [[ -n $SSH_AUTH_SOCK && ! -S $SSH_AUTH_SOCK ]] && (( $EPOCHSECONDS - $_nmk_update_ssh_socket_tmux_last_check > 60 )) && {
-        eval "$(tmux show-environment -s)"
-        echo >&2 "prompt-hook ($0): environment updated from tmux session"
+    local env_lines
+    local socket_line
+    local socket_path
+    local tmux_output
+    local tmux_outputs
+    local update_env_command
+    (( $EPOCHSECONDS - $_nmk_update_ssh_socket_tmux_last_check > 5 )) \
+        && [[ -n $SSH_AUTH_SOCK && ! -S $SSH_AUTH_SOCK ]] && {
         _nmk_update_ssh_socket_tmux_last_check=$EPOCHSECONDS
+        tmux_output="$(tmux show-environment \; display-message -p -- ----marker---- \; show-environment -s 2>/dev/null)"
+        if (( $? )); then
+            return $?
+        fi
+        tmux_outputs=("${(@s:----marker----:)tmux_output}")
+        env_lines=(${(f)"${tmux_outputs[1]}"})
+        socket_line="${env_lines[(r)SSH_AUTH_SOCK=*]}"
+        [[ -n $socket_line ]] && {
+            socket_path=${socket_line#SSH_AUTH_SOCK=}
+            if [[ $socket_path == $SSH_AUTH_SOCK ]]; then
+                # unchanged, do nothing
+                return 0
+            fi
+            [[ -S $socket_path ]] && {
+                update_env_command=${tmux_outputs[2]}
+                eval "$update_env_command"
+                echo >&2 "prompt-hook ($0): environment updated from tmux session"
+            }
+        }
     }
 }
 
+typeset -g _nmk_update_ssh_socket_vscode_last_check=0
 _nmk-update-ssh-socket-vscode() {
+    local socket
     local -a sockets
     local cur_socket
-    [[ -n $SSH_AUTH_SOCK && ! -S $SSH_AUTH_SOCK ]] && {
+    (( $EPOCHSECONDS - $_nmk_update_ssh_socket_vscode_last_check > 5 )) \
+        && [[ -n $SSH_AUTH_SOCK && ! -S $SSH_AUTH_SOCK ]] && {
         cur_socket=$SSH_AUTH_SOCK
         # see https://zsh.sourceforge.io/Doc/Release/Expansion.html#Glob-Qualifiers
         # O = files owned by the effective user ID
         # om = sort by last modification, youngest first
         sockets=(/tmp/vscode-ssh-auth-*(Uom))
-        for socket in ${sockets[@]}; do
+        # the first socket should be alive but we check first 3 sockets
+        for socket in ${sockets[@]:0:3}; do
             if [[ -S $socket ]]; then
                 export SSH_AUTH_SOCK=$socket
                 echo >&2 "$0: SSH_AUTH_SOCK changed from $cur_socket to $socket"
-                return 0
+                break
             fi
         done
+        _nmk_update_ssh_socket_vscode_last_check=$EPOCHSECONDS
     }
 }
 
@@ -46,17 +75,16 @@ _nmk-update-ssh-socket-vscode() {
     _nmk_preexec_functions+=_nmk-kubectl-preexec
 }
 
+# register a hook running on every prompt line
 [[ -n $SSH_AUTH_SOCK ]] && {
     # tmux session on remote server
     if [[ -n $TMUX && -n $SSH_CONNECTION ]]; then
-        # register a hook running on every prompt line
         _nmk_precmd_functions+=_nmk-update-ssh-socket-tmux
     elif [[ $TERM_PROGRAM == vscode ]]; then
         if [[ $SSH_AUTH_SOCK == /tmp/vscode-ssh-auth-* ]]; then
             # vscode on remote server or devcontainer
             if [[ $REMOTE_CONTAINERS == true ]] || [[ -n $SSH_CONNECTION ]]; then
-                # runs on shell start (new terminal/revived terminal)
-                _nmk-update-ssh-socket-vscode
+                _nmk_precmd_functions+=_nmk-update-ssh-socket-vscode
             fi
         fi
     fi
