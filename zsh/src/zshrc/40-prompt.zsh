8a8ef04ec47c96ef6415af2f0670414ef39be321
Nui Narongwet <narongwet.m@gmail.com>
2025-08-08 16:42:06 +0700

    zsh: Do not hide hostname in remote containers

--- zsh/src/zshrc/40-prompt.zsh
+++ zsh/src/zshrc/40-prompt.zsh
@@ -9,8 +9,10 @@
         [[ -n $TERM ]] && (( ${+commands[tput]} )) && (( $(command tput colors) < 256 )) && horizontal_branch_symbol=
 
         # Hide user and host in prompt if NMK_DEVELOPMENT is true by default,
-        # this is not apply to zsh in ssh session
-        [[ $NMK_DEVELOPMENT == true && -z $SSH_TTY ]] && horizontal[show_user_and_host]=0
+        # this is not apply to zsh in ssh session and shell running in devcontainer
+        [[ $NMK_DEVELOPMENT == true && -z $SSH_TTY && $REMOTE_CONTAINERS != true ]] && {
+            horizontal[show_user_and_host]=0
+        }
 
         # Change prompt color on remote session
         if [[ -n $SSH_TTY || $SUDO_USER == ssm-user ]]; then
