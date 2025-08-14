792e04c9f98b26b894d6027e13cd89aaa2dc15ec
Nui Narongwet <narongwet.m@gmail.com>
2025-08-14 21:48:08 +0700

    Update login.sh and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,78 +1,112 @@
-#!/bin/sh
-
-# A helper script to start login shell.
+# An entrypoint for ssh authorized_keys command option
 #
-# Compatibility: dash, bash, and zsh
+# Intended usage
+#   - as a command in authorized_keys "command" option
+#
+# Note
+#   - This script should be sourced
+#   - All positional arguments are ignored
+#   - we can't use "exec -l" because busybox exec doesn't support it
 
 if [ -n "$ZSH_VERSION" ]; then
-    # make zsh behave like bash on word spliting
-    setopt sh_word_split
+    # make zsh compatible with posix shell
+    emulate sh
 fi
 
-try_exec() {
-    local command
-    local flags
-    local launcher
-    launcher="$1"
-    command="$SSH_ORIGINAL_COMMAND"
+LAUNCHER_PATH=""
+SHELL_PROG=""
 
+# Determine shell program that source this script
+if [ -n "$BASH_VERSION" ]; then
+    SHELL_PROG=bash
+elif [ -n "$ZSH_VERSION" ]; then
+    SHELL_PROG=zsh
+else
+    SHELL_PROG="$0"
+fi
+
+locate_launcher() {
+    launcher="$1"
     # return if file doesn't exist or broken symlink
     if [ ! -e "$launcher" ]; then
-        return 0
+        return 1
     fi
-
     # return if file is not executable (for some reason)
     if [ ! -x "$launcher" ]; then
         >&2 echo "$launcher" is not executable
-        return 0
+        return 1
+    fi
+    LAUNCHER_PATH="$launcher"
+    unset launcher
+}
+
+fallback_no_launcher() {
+    # If SSH_ORIGINAL_COMMAND is set, execute it
+    if [ -n "$SSH_ORIGINAL_COMMAND" ]; then
+        exec "$SHELL_PROG" -c "$SSH_ORIGINAL_COMMAND"
     fi
 
-    if [ -n "$command" ]; then
-        exec "$launcher" exec --set-shell --eval-cmd "$command"
-    else
-        if [ -n "$SSH_CONNECTION" ]; then
-            flags="$flags --motd"
-        fi
-        exec "$launcher" $flags --login
+    exec "$SHELL_PROG" -l
+}
+
+unexport_ssh_original_command() {
+    # This script should not expose SSH_ORIGINAL_COMMAND to children
+    if [ -n "$SSH_ORIGINAL_COMMAND" ]; then
+        _cmd="$SSH_ORIGINAL_COMMAND"
+        unset SSH_ORIGINAL_COMMAND
+        SSH_ORIGINAL_COMMAND="$_cmd"
+        unset _cmd
     fi
+}
+
+# See https://www.shellcheck.net/wiki/SC3050
+escape() { printf "'%s'\\n" "$(printf '%s' "$1" | sed -e "s/'/'\\\\''/g")"; }
+
+
+execute_specified_command() {
+    # handle SSH_ORIGINAL_COMMAND base on following conditions
+    #   - If it is a shell, update SHELL environment variable to zsh (if available).
+    #     Tools that detect login shell from that variable will work properly
+    #   - if "nmk" (the launcher), call it using absolute path
+    #   - otherwise, evalute specified command
+    case "$SSH_ORIGINAL_COMMAND" in
+        # a shell without arguments
+        sh | bash | zsh )
+            exec "$LAUNCHER_PATH" exec --set-shell "$SSH_ORIGINAL_COMMAND"
+            ;;
+        # a shell with arguments
+        sh[[:space:]]* | bash[[:space:]]* | zsh[[:space:]]* )
+            exec "$LAUNCHER_PATH" exec --set-shell --eval-cmd "$SSH_ORIGINAL_COMMAND"
+            ;;
+        nmk )
+            exec "$LAUNCHER_PATH"
+            ;;
+        nmk[[:space:]]* )
+            escaped_launcher_path=$(escape "$LAUNCHER_PATH")
+            exec "$SHELL_PROG" -c "$escaped_launcher_path ${SSH_ORIGINAL_COMMAND#nmk[[:space:]]}"
+            ;;
+        # excute SSH_ORIGINAL_COMMAND
+        * )
+            exec "$SHELL_PROG" -c "$SSH_ORIGINAL_COMMAND"
+            ;;
+    esac
 }
 
 main() {
-    local current_exe
-    local global_nmk
-    local login_flag
+    unexport_ssh_original_command
 
-    try_exec "${NMK_HOME:-$HOME/.nmk}/bin/nmk"
+    locate_launcher "$NMK_LAUNCHER_PATH" \
+        || locate_launcher "${NMK_HOME:-$HOME/.nmk}/bin/nmk" \
+        || locate_launcher "$(command -v nmk 2>/dev/null)" \
+        || fallback_no_launcher
 
-    global_nmk="$(command -v nmk 2>/dev/null)"
-    if [ -x "$global_nmk" ]; then
-        try_exec "$global_nmk"
-    fi
+    [ -n "$SSH_ORIGINAL_COMMAND" ] && execute_specified_command
 
-
-    # If this line is reached, we didn't find candidate launcher
-
-    # If command is set, execute it
-    if [ -n "$SSH_ORIGINAL_COMMAND" ]; then
-        eval "$SSH_ORIGINAL_COMMAND"
-        exit $?
-    fi
-
-    # fallback to re-execute itself.
-    # N.B. MacOs doesn't have procfs, we fallback to /bin/sh
-    current_exe="$(readlink -f /proc/$$/exe)"
-    if [ ! -e "$current_exe" ]; then
-        current_exe=/bin/sh
-    fi
-
-    # Bash and zsh support exec with login flag
-    case "$current_exe" in
-        */bash | */zsh) login_flag=-l ;;
-        *) ;;
-    esac
-
-    exec $login_flag "$current_exe"
+    # Default to start a login shell
+    exec "$LAUNCHER_PATH" --motd --login
 }
 
-main "$@"
+# NOTE: we don't specify "$@" because this entrypoint should not be called with arguments
+main
+
 # vi: ft=sh
--- login.sh
+++ login.sh
@@ -1,63 +1,58 @@
 #!/bin/sh
 
-# A helper script to start login shell.
+# An entrypoint for ssh authorized_keys command option
 #
-# Compatibility: dash, bash, and zsh
+# Intended usage
+#   - as a command in authorized_keys "command" option
+#   - call manually
+#
+# This script shouldn't handle SSH_ORIGINAL_COMMAND because it is managed by the caller
 
-# This make bash not put following commands in history when sourcing this file
+# Tell bash and zsh to not put following commands in history when sourcing this file
 unset HISTFILE
 
 if [ -n "$ZSH_VERSION" ]; then
-    # make zsh behave like bash on word spliting
-    setopt sh_word_split
+    # make zsh compatible with posix shell
+    emulate sh
 fi
 
-try_start() {
-    local flags
-    local launcher
-    launcher="$1"
-    shift 1
+LAUNCHER=""
 
+locate_launcher() {
+    launcher="$1"
     # return if file doesn't exist or broken symlink
     if [ ! -e "$launcher" ]; then
-        return
+        return 0
     fi
-
     # return if file is not executable (for some reason)
     if [ ! -x "$launcher" ]; then
         >&2 echo "$launcher" is not executable
-        return
+        return 0
     fi
+    LAUNCHER="$launcher"
+}
+
+no_launcher_handler() {
+    # fallback to shell
+    [ -n "$BASH_VERSION" ] && exec bash -l
+    [ -n "$ZSH_VERSION" ]  && exec zsh -l
+
+    exec "${SHELL:-sh}" -l
+}
+
+main() {
+    locate_launcher "${NMK_HOME:-$HOME/.nmk}/bin/nmk"
+    [ -z "$LAUNCHER" ] && locate_launcher "$(command -v nmk 2>/dev/null)"
+    # If we didn't find a launcher
+    [ -z "$LAUNCHER" ] && no_launcher_handler
 
     flags="--login"
     if [ -n "$SSH_CONNECTION" ]; then
         flags="$flags --motd"
     fi
-
-    # transfer execution to launcher
-    exec "$launcher" $flags "$@"
+    exec "$LAUNCHER" $flags "$@"
 }
 
-try_start "${NMK_HOME:-$HOME/.nmk}/bin/nmk" "$@"
+main "$@"
 
-global_nmk=$(command -v nmk 2>/dev/null)
-if [ -x "$global_nmk" ]; then
-    try_start "$global_nmk" "$@"
-fi
-
-# If this line is reached, we didn't find candidate launcher, fallback to re-execute itself.
-# N.B. MacOs doesn't have procfs, we fallback to /bin/sh
-current_exe=$(readlink -f /proc/$$/exe)
-if [ ! -e "$current_exe" ]; then
-    current_exe=/bin/sh
-fi
-
-login_flag=""
-# Bash and zsh support exec with login flag
-case "$current_exe" in
-    */bash | */zsh) login_flag=-l ;;
-    *) ;;
-esac
-
-exec $login_flag "$current_exe"
 # vi: ft=sh
