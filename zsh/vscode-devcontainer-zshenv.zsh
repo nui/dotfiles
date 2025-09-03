1df8314b57ea48d2564359ed0327584759d1945f
Nui Narongwet <narongwet.m@gmail.com>
2025-09-03 21:43:54 +0700

    update vscode-devcontainer-zshenv.zsh

--- zsh/vscode-devcontainer-zshenv.zsh
+++ zsh/vscode-devcontainer-zshenv.zsh
@@ -1,17 +1,39 @@
+# Uncomment this code to debug this script problem
+# (
+#     echo ">> date: $(date)"
+#     echo ">> cmdline"
+#     tr '\0' '\n' < /proc/$$/cmdline
+#     echo ""
+#     echo ">> printenv"
+#     printenv
+#     echo ""
+#     echo ">> declare"
+#     declare
+#     echo ""
+# ) >> ~/vscode-devcontainer.log
+
+
 # How to use this function
 #   - source this file from .zshenv at very beginning of file
 #   - or symbolic link .zshenv to this file
 #
-#
 # A helper function to set ZDOTDIR to DEVCON_<USERNAME>_ZDOTDIR in devcontainer environment
 function {
     setopt localoptions noshwordsplit
-    local devcon_zdotdir
 
+    # userEnvProbe runs on zsh shell with one or both of following options
+    #   - login
+    #   - interactive
+    if ! [[ -o login || -o interactive ]]; then
+        return 0
+    fi
+
+    # "-n $ZDOTDIR" prevents recursively source this file if ZDOTDIR is set to home directory
     if [[ -n $ZDOTDIR || -z $REMOTE_CONTAINERS_IPC ]]; then
         return 0
     fi
 
+    local devcon_zdotdir
     devcon_zdotdir="DEVCON_${(U)USERNAME}_ZDOTDIR"
     if [[ ${(P)devcon_zdotdir+set} = set ]]; then
         local zdotdir=${(P)devcon_zdotdir}
@@ -33,7 +55,7 @@ function {
                 flags+=c
 
                 # get path to binary of zsh running this script
-                # we don't need to support macOS, because devcontainer runs in linux only
+                # we don't need to support macOS, because devcontainer runs on linux only
                 local zsh_path
                 zsh_path=/proc/self/exe
                 # :A resolve symbolic links
