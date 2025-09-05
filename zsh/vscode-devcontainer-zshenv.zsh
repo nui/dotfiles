aa5a40f460d0a04bf09b5c1d2ab9091650b9b82a
Nui Narongwet <narongwet.m@gmail.com>
2025-09-05 18:19:56 +0700

    horizontal: do not show error code by default

--- zsh/vscode-devcontainer-zshenv.zsh
+++ zsh/vscode-devcontainer-zshenv.zsh
@@ -24,13 +24,16 @@ function {
     setopt localoptions noshwordsplit
 
     # userEnvProbe runs on zsh shell with one or both of following options
-    #   - login
     #   - interactive
-    if [[ ! -o login && ! -o interactive ]]; then
+    #   - login
+    # return if the current shell is non-interactive non-login shell
+    if [[ ! -o interactive && ! -o login ]]; then
         return 0
     fi
 
-    # "-n $ZDOTDIR" prevents recursively source this file if ZDOTDIR is set to home directory
+    # return if $ZDOTDIR is set or not running under vscode
+    #
+    # The only case when ZDOTDIR is set, is when it is set to HOME directory
     if [[ -n $ZDOTDIR || -z $REMOTE_CONTAINERS_IPC ]]; then
         return 0
     fi
@@ -40,13 +43,19 @@ function {
     if [[ -z $zdotdir || ! -d $zdotdir ]]; then
         return 0
     fi
+
     # vscode-server call zsh to determine environment variable using command with following format
     #
     #   zsh -lic "echo -n $UUID; cat /proc/self/environ; echo -n $UUID"
     #
     # $UUID is a random uuid
     #
-    # it may use -lic, -lc or -ic flags, depends on userEnvProbe value
+    # it may use -lic, -lc or -ic, depends on userEnvProbe value
+    #
+    # There is no guarantee that vscode will always use this format
+    # However, it is very unlikely to be changed
+
+    # check if we are called by vscode to determine user environment variabls
     if [[ $ZSH_EXECUTION_STRING != "echo -n "* ]]; then
         return 0
     fi
@@ -59,23 +68,23 @@ function {
     # at this point, all test conditions are satisfied.
 
     # construct correct zsh flags by testing options
-    local flags=-
+    local zsh_flags=-
     if [[ -o login ]]; then
-        flags+=l
+        zsh_flags+=l
     fi
     if [[ -o interactive ]]; then
-        flags+=i
+        zsh_flags+=i
     fi
-    flags+=c
+    zsh_flags+=c
 
-    # resolve binary path of current running zsh
+    # resolve binary path of current running zsh using procfs
     # we don't need to support macOS, devcontainer runs on linux only
     local zsh_path=/proc/self/exe
     # :A resolve symbolic links
     zsh_path=${zsh_path:A}
 
     local -a args
-    args=($zsh_path $flags $ZSH_EXECUTION_STRING)
+    args=($zsh_path $zsh_flags $ZSH_EXECUTION_STRING)
 
     # re-execute this command again under correct environment
 
@@ -84,6 +93,7 @@ function {
 
     # if the launcher is found and executable bit is set, use it
     if [[ -x $launcher ]]; then
+        # --no-log is optional but we don't really need logging here
         exec $launcher --no-log iexec ${args[@]}
     # otherwise, set ZDOTDIR and execute
     else
