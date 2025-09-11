27250d1772dc01aefa67c2c6a2f39b3a3311b8ab
Nui Narongwet <narongwet.m@gmail.com>
2025-09-11 22:35:19 +0700

    Add template for cargo

---
+++ template/files/cargo.sh
@@ -0,0 +1,9 @@
+# provide binstall subcommand
+cargo install cargo-binstall
+
+# provide upgrade command subcommand
+cargo install cargo-edit
+
+# provide install-update subcommand
+cargo install cargo-update
+
