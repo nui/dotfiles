8fa36cd6a13f9de4095c220576461d27f42e3d28
Nui Narongwet <narongwet.m@gmail.com>
2025-12-01 19:11:21 +0700

    zshrc: Add nmk_clap_dynamic_completions to control which completion file is read

--- zsh/src/zshrc/30-clap-dynamic-completion.zsh
+++ zsh/src/zshrc/30-clap-dynamic-completion.zsh
@@ -1,7 +1,19 @@
+# set default value if nmk_clap_dynamic_completions is unset
+(( ! ${+nmk_clap_dynamic_completions} )) && {
+    typeset -ga nmk_clap_dynamic_completions
+    nmk_clap_dynamic_completions=(nmk nmkup)
+}
+
 () {
     local file
-    for file ($ZDOTDIR/zshrc.clap-dynamic-completion.d/*.zsh(N)) {
-        # we need zsh-defer here to make it works on linux
-        [[ -e $file ]] && zsh-defer source $file
+    local name
+    for name ($nmk_clap_dynamic_completions) {
+        (( ${+commands[$name]} )) && {
+            file=$ZDOTDIR/zshrc.clap-dynamic-completion.d/${name}.zsh
+            # we need zsh-defer to make dynamic completion work on linux
+            # no idea why it doesn't work
+            [[ -e $file ]] && zsh-defer source $file
+        }
     }
 }
+
