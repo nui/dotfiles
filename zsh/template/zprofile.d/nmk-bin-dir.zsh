5e4637af61be069dcdd1daaad99113fe41f60089
Nui Narongwet <narongwet.m@gmail.com>
2026-02-01 06:34:42 +0700

    Use zsh array inverse

--- zsh/template/zprofile.d/nmk-bin-dir.zsh
+++ zsh/template/zprofile.d/nmk-bin-dir.zsh
@@ -1,12 +1,15 @@
 () {
     # Initialize path if not start from our launcher
     if [[ ! -e $NMK_LAUNCHER_PATH ]]; then
+        local -a bin_dirs
         local dir
         local name
-        for name in local/bin devbin bin; do
+        bin_dirs=(bin devbin local/bin)
+        # Oa = reverse order
+        for name in ${(Oa)bin_dirs}; do
             dir=$NMK_HOME/$name
-            # if $dir is not in path array
-            if [[ ${path[(Ie)$dir]} -eq 0 ]]; then
+            # If $dir is not present in the path array and it does exist.
+            if [[ ${path[(Ie)$dir]} -eq 0 && -d $dir ]]; then
                 path=($dir $path)
             fi
         done
