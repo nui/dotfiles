e7d944a3254524a45294e9f657fc4c3cc4cc8fa8
Nui Narongwet <narongwet.m@gmail.com>
2026-02-02 08:07:37 +0700

    move vscode path hack to template directory

--- zsh/template/zshrc.pre.d/vscode-macos-path-hack.zsh
+++ zsh/template/zshrc.pre.d/vscode-macos-path-hack.zsh
@@ -1,4 +1,4 @@
-if [[ $OSTYPE == darwin* ]] && [[ $TERM_PROGRAM == vscode ]]; then
+if [[ -z $NMK_LAUNCHER_PATH ]] && [[ $OSTYPE == darwin* ]] && [[ $TERM_PROGRAM == vscode ]]; then
     # vscode on macOS move /usr/local/bin to the front of path.
     # We put $NMK_HOME/bin to the front of path to fix this annoying issue.
     path=($NMK_HOME/bin $path)
