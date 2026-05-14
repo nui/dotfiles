a6f5afbfe64bb941c1e434486a3862a6c77d282b
Nui Narongwet <narongwet.m@gmail.com>
2026-05-14 13:16:47 +0700

    Update lvm book

--- launcher/book/lvm.sh
+++ launcher/book/lvm.sh
@@ -1,2 +1,12 @@
 # Enlarge logical volume by 10G
 lvresize -L +10G vg-name/lv-name
+
+# Create a new volume on thin pool with virtual size 1G
+lvcreate -V1G -T vg-main/microvm-pool --name foo
+
+# Create a snapshot
+lvcreate -s -n foo-snapshot vg-main/foo
+
+# Restore from snapshot
+lvconvert --merge vg-main/foo-snapshot
+
